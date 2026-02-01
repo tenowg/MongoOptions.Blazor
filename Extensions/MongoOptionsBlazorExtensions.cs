52bb36ad3837e361a9acead7efe504f98c8ba677
Craig Russell <tenowg@gmail.com>
2026-02-01 01:05:33 -0500

    Initial Commit

---
+++ Extensions/MongoOptionsBlazorExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace MongoOptions.Blazor.Extensions
+{
+    public static class MongoOptionsBlazorExtensions
+    {
+        public static IServiceCollection AddMongoConfigurationUI(this IServiceCollection services)
+        {
+            // This is a great place to register any UI-specific 
+            // services or JS interop helpers you might add later
+            return services;
+        }
+    }
+}
